1c95c0c058b564d3eec341006271b3c5b440a302
Stefan Tagarski <116569281+stefantagarski@users.noreply.github.com>
2023-11-19 21:16:14 +0100

    Add files via upload

---
+++ lab4/zadaca3/SumOfAbsoluteDifferences.java
@@ -0,0 +1,44 @@
+package zadaca3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SumOfAbsoluteDifferences {
+
+    static int solve(int[] numbers, int N, int K) {
+        int[][] matrix = new int[N][K];
+
+        int max = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < K; j++) {
+                for (int p = 0; p < i; p++) {
+                    int absoluteDifference = Math.abs(numbers[i] - numbers[p]);
+                    matrix[i][j] = Math.max(matrix[i][j], matrix[p][j - 1] + absoluteDifference);
+                }
+                max = Math.max(matrix[i][j], max);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int res = solve(numbers, N, K);
+        System.out.println(res);
+
+        br.close();
+    }
+}
